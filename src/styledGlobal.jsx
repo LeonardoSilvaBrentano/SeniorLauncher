a6e101df8ff8291a569070be210e933513aea158
LeonardoSilvaBrentano <brentanoleo@hotmail.com>
2023-08-01 12:19:04 -0300

    0108

--- src/styledGlobal.jsx
+++ src/styledGlobal.jsx
@@ -10,6 +10,7 @@ export const StyledGlobal = createGlobalStyle`
 `
 
 export const CorFundo = styled.div`
-    background: linear-gradient(130deg, #F9D03F -80%, #171616 30%);
+    /* background: linear-gradient(130deg, #F9D03F -80%, #171616 30%); */
+    background: white;
     height: 100vh;
 `
